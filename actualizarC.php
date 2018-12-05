3001a9be4e97eafcc3538025b4db48d7f72d27db
zappy-zero <color.ui>
2018-12-05 18:07:12 -0500

    Probando3

--- actualizarC.php
+++ actualizarC.php
@@ -7,5 +7,10 @@ $producto = new Modelo();
 $codigo = $_REQUEST['actualizar'];
 
     $cod = $producto->getProducto($codigo);
+    
+    if($cod!=null){
     $_SESSION['producto'] = serialize($cod);
     header('Location: ./actualizar.php');
+    } else {
+        header('Location: ./index.php');
+}
